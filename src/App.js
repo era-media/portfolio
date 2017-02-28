45e8a740c57e831f2da1f73e118da431bf2b9778
Muhammad Moaz <moaz.aamir@gmail.com>
2017-02-28 17:27:37 +0300

    Initialize page piling effect

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
+        <div id="pagepiling">
+            <div className="section">Section 1</div>
+            <div className="section">Section 2</div>
+            <div className="section">Section 3</div>
+            <div className="section">Section 4</div>
+        </div>
       </div>
     );
   }
