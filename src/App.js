bfc9d78c7304230ab3eb176f6ad9c8af875b2c50
Muhammad Moaz <moaz.aamir@gmail.com>
2017-03-01 11:04:26 +0300

    Add fonts

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </div>
         <div id="pagepiling">
-            <div className="section">Section 1</div>
-            <div className="section">Section 2</div>
-            <div className="section">Section 3</div>
-            <div className="section">Section 4</div>
+            <div className="section"></div>
+            <div className="section"></div>
+            <div className="section"></div>
+            <div className="section"></div>
         </div>
       </div>
     );
